14ef2766d89518059701921e295484b50a33ed59
Thómas Jéfferson <thomasjteixeira@gmail.com>
2023-11-22 16:49:31 -0300

    partial search bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
-import { Hero } from '@/components'
+import { Hero, SearchBar, CustomFilter } from '@/components'
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+
+      <div className="mt-12 padding-x padding-y max-width" id="discorver">
+        <div className="home__text-container">
+          <h1 className="text-4x1 font-extrabold"> Catálogo de Carros</h1>
+          <p> Explore os carros que você pode gostar</p>
+        </div>
+        <div className="home__filters">
+          <SearchBar />
+          <CustomFilter title="fuel" />
+          <CustomFilter title="year" />
+        </div>
+      </div>
     </main>
   )
 }
